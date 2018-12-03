6a0896af4a328debaedec0643031fd89f4ec983b
Senthil Nayagan <senthil.nayagan@gmail.com>
2018-12-03 04:46:45 -0500

    Create HelloWorld.scala

---
+++ src/main/scala/org/earlycode/scalatutorial/basics/HelloWorld.scala
@@ -0,0 +1,10 @@
+package org.earlycode.scalatutorial.basics
+
+object HelloWorld {
+  /**
+    * Main method which acts as the entry point of our program.
+    */
+  def main(args: Array[String]): Unit = {
+    println("Hello World!")
+  }
+}

c1221a3b152ebef81db0aae04f7eb2c0cdf80d7b
senthilnayagan <senthil.nayagan@gmail.com>
2018-12-06 21:27:14 +0530

    New inclusion

---
+++ src/main/scala/org/earlycode/scalatutorial/basics/TypeInferenceExamples.scala
@@ -0,0 +1,11 @@
+package org.earlycode.scalatutorial.basics
+
+object TypeInferenceExamples extends App {
+  // Scala compiler figures out the type of the variable based
+  // on the value assigned to it.
+  // Thus, myVal will of String type, myVar will be Int type and
+  // myBoolean will of boolean type.
+  val myVal = 10
+  var myVar = "SpaceX"
+  var myBoolean = true
+}

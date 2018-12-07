25c316e94aeeeb000bca8697405bd152d31eff41
senthilnayagan <senthil.nayagan@gmail.com>
2018-12-07 10:10:56 +0530

    Updates and new inclusions

---
+++ src/main/scala/org/earlycode/scalatutorial/advanced/PassFuncAsArguments.scala
@@ -0,0 +1,10 @@
+package org.earlycode.scalatutorial.advanced
+
+object PassFuncAsArguments extends App {
+  /**
+    * This feature is called Higher-order Function where both
+    * methods and functions can take functions as parameters or
+    * returns a function as its result.
+    */
+
+}

b6c182fb8ef7db05d00c22fc25f29a6a79772838
senthilnayagan <senthil.nayagan@gmail.com>
2018-12-08 09:12:12 +0530

    Updates and new inclusions

--- src/main/scala/org/earlycode/scalatutorial/advanced/PassFuncAsArguments.scala
+++ src/main/scala/org/earlycode/scalatutorial/advanced/PassFuncAsArguments.scala
@@ -1,22 +1,29 @@
 package org.earlycode.scalatutorial.advanced
 
+
+/**
+  * This feature is called Higher-order Function where both
+  * methods and functions can take functions as parameters or
+  * returns a function as its result.
+  */
 object PassFuncAsArguments {
   /**
-    * This feature is called Higher-order Function where both
-    * methods and functions can take functions as parameters or
-    * returns a function as its result.
+    * Here, this delayed method accepts another method as argument.
     */
-  def delayed(callBack: => Long): Unit = {
+  def delayed(callBack: () => Long): Unit = {
     println("In delayed() method.")
-    println("Time: " + callBack)
+    println("Time: " + callBack())
   }
 
+  // This method returns nano time as long integer type.
   def time(): Long = {
     System.nanoTime
   }
 
 
-  // Calls delayed function with a callback function.
+  // Calls delayed function with passing an another
+  // method as argument. The method being passed doesn't take
+  // any argument, but returns a long integer.
   def main(args: Array[String]): Unit = {
     delayed(time)
   }

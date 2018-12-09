3eecd3a67a8b4ac1d9dfe9402826c8371601fb48
senthilnayagan <senthil.nayagan@gmail.com>
2018-12-09 21:02:47 +0530

    Updates and new inclusions

---
+++ src/main/scala/org/earlycode/scalatutorial/fp/PatternMatchingDemo.scala
@@ -0,0 +1,26 @@
+package org.earlycode.scalatutorial.fp
+
+object PatternMatchingDemo {
+  def main(args: Array[String]): Unit = {
+    println(numToString(3))
+  }
+
+
+  /**
+    * The last case _ is a “catch all” case which matches anything
+    * not defined in the above cases.
+    *
+    * Note that if we try to put a case _ before any other case
+    * clauses, the compiler will throw an "unreachable code" error
+    * on the next clause. Nothing will get passed the case _ clause
+    * and hence, case _ serves as default.
+    */
+  def numToString(num: Int): String = num match {
+    case 2 => "two"
+    case 1 => "one"
+    case 3 => "three"
+
+    // Default case clause
+    case _ => "other"
+  }
+}

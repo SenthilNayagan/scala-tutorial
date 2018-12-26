54bcfdfcf53a2b97a135e92831646e7e0ee26a54
senthilnayagan <senthil.nayagan@gmail.com>
2018-12-26 12:22:44 +0530

    Updates and new inclusions

--- src/main/scala/org/earlycode/scalatutorial/basics/AbstractClassDemo.scala
+++ src/main/scala/org/earlycode/scalatutorial/basics/AbstractClassDemo.scala
@@ -2,28 +2,50 @@ package org.earlycode.scalatutorial.basics
 
 
 abstract class Pet(name: String) {
-  var age: Int = 0              // concrete field
+  // Concrete field and we must provide an initial value.
+  var age: Int = 2
 
-  def printName: Unit           // abstract method (no body makes this method abstract)
+  // Abstract field which has to be implemented by its subclass. Otherwise, the
+  // the extending class must be declared "abstract".
+  val breed: String
 
-  def printAge: Unit = {        // concrete method
+  // Abstract method - no body makes this method abstract.
+  def printName: Unit
+
+  // Abstract method which has to be implemented by its subclass.
+  //def printAll: String
+
+  // Concrete method
+  def printAge: Unit = {
     println(age)
   }
+
+  def color: String
 }
 
 class Dog(name: String) extends Pet(name) {
+  // Implementation of abstract filed defined in the above abstract bass class.
+  val breed = "Boxer"
+
   // Implementation of abstract method
   def printName: Unit = println("Hi there, I am " + name)
 
-  // Overrides the implemented method to add more details
+  // Overrides the concrete method of base abstract class to add more details to it
   override def printAge: Unit = {
     println(name + "'s age is " + age)
   }
+
+  val color = "Brown"
+
+  //val printAll = "Name: " + name + "; " + "Age: " + age + "; " + "Breed: " + breed + "; " + "Color: " + color
 }
 
 object AbstractClassDemo extends App {
   val dogObj = new Dog("Jimmy")
   dogObj.printName
-  dogObj.age = 2
   dogObj.printAge
+  dogObj.age = 5
+  dogObj.printAge
+  //println(dogObj.printAll)
+  println(dogObj.color)
 }
